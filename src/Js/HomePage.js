5829e65b6ef8386641b6d34928c7703abb0e89e6
HuynhDucPNV25 <143483558+HuynhDucPNV25@users.noreply.github.com>
2023-10-06 19:27:38 +0700

    Huỳnh Đức HomePage (#2)

--- src/Js/HomePage.js
+++ src/Js/HomePage.js
@@ -12,7 +12,7 @@ const callAPI = () => {
 
 // Hàm hiển thị dữ liệu vào thẻ <select>
 const renderData = (array, select) => {
-  let row = '<option disabled value="">Chọn</option>';
+  let row = '';
   array.forEach(element => {
     row += `<option data-id="${element.code}" value="${element.name}">${element.name}</option>`;
   });
@@ -25,12 +25,15 @@ $(document).ready(function() {
 
   // Khởi tạo thẻ <select> và văn bản của thành phố
   $("#agency").text(selectedCity);
-  $("#city").val(selectedCity).filter("option[value='" + selectedCity + "']").attr("selected", true);
-
+  $("#city").val(selectedCity);
+  $("#topSalon").text("Top salon tại "+(selectedCity));
+  $("#topMauToc").text("Top mẫu tóc hót tại "+(selectedCity));
   // Xử lý sự kiện thay đổi của thẻ <select>
   $("#city").change(() => {
     selectedCity = $("#city option:selected").text();
     $("#agency").text(selectedCity).attr("data-agency", selectedCity);
+    $("#topSalon").text(("Top salon tại ")+selectedCity).attr("data-agency", selectedCity);
+    $("#topMauToc").text(("Top mẫu tóc hót tại ")+selectedCity).attr("data-agency", selectedCity);
     localStorage.setItem("selectedCity", selectedCity);
   });
 
@@ -60,8 +63,42 @@ items.forEach(item => {
     });
 
     // Thêm lớp "active" cho item được nhấp chuột
-    item.classList.remove('inactive');
     item.classList.add('active');
+    item.classList.remove('inactive');
   });
 });
+//......... 
 
+
+const slideData = [
+  { imageSrc: "../../image/pd1.png" },
+  { imageSrc: "../../image/pd2.png" },
+  { imageSrc: "../../image/GGG 2.png" }
+];
+
+// Lấy container trong carousel để chứa các slide
+const carouselInner = document.getElementById("carouselInner");
+
+// Tạo và thêm các slide vào container
+slideData.forEach(function(slide, index) {
+  // Tạo phần tử slide
+  const slideElement = document.createElement("div");
+  slideElement.classList.add("carousel-item");
+  if (index === 0) {
+    slideElement.classList.add("active");
+  }
+  slideElement.innerHTML = `
+    <div>
+      <img src="${slide.imageSrc}" alt="">
+    </div>
+  `;
+  
+  // Thêm slide vào container
+  carouselInner.appendChild(slideElement);
+});
+
+
+
+// .............
+
+// Dữ liệu các card
